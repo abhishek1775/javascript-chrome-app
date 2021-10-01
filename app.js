eb79c6d01a3b797dde4411c779c6d0bcecbcfef9
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-01 21:12:31 +0900

    #4.2 Events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const userName = loginInput.value;
-    console.log(userName);
+function onLoginSubmit(event) { //event object를 담은 정보를 인수로 받음.
+    event.preventDefault(); //event의 기본행동이 발생되지 않도록 함. (submit으로 인한 새로고침 안되게끔)
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit); //JS는 event가 발생하면 function에 event object를 argument로 줌.
