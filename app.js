e5c61a658495eef551bc2d8e39cbdf124c814b98
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-02 00:34:42 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KET = "username";
 
 function onLoginSubmit(event) { 
     event.preventDefault(); 
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username); // key, value
-    //localStorage은 브라우저의 저장소. .setItem, .getItem, .removeItem 등 사용.
+    localStorage.setItem(USERNAME_KET, username); 
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit); 
+const saveUsername = localStorage.getItem(USERNAME_KET);
+
+if(saveUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit); 
+} else {
+    paintGreetings(saveUsername);
+}
