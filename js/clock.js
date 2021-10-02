5c2c09a9f72e242e35be20388b6aa8873fd1e18a
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-02 11:57:40 +0900

    #5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date()
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0"); //Number을 String으로 바꾼뒤, 두자리가 안되면 앞을 "0"으로 채움.
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
