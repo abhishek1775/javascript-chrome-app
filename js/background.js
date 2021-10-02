0c6d1408b6501bc42bc570f728ffca93c7b0af49
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-02 14:25:25 +0900

    #6.1 Background

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.JPG", "1.JPG", "2.JPG"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); //element추가
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage); //HTML body에 추가
