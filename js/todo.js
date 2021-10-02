56fd90de69102bb53bdb0d02e18e3924e040e519
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-02 22:24:57 +0900

    #7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement; //클릭된 버튼(target)의 부모 엘리먼트
+    li.remove(); //리스트 삭제
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
