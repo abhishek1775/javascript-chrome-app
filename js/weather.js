99cfa42d823d00311151dc35f4ec66bb12731766
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-04 00:46:47 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "577dc4e69d991b157c442575f0584f9d";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
