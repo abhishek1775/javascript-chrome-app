0c5c2c2b93cd026e3d785b10f8fdebfa0531b02c
abhishek1775 <87231462+abhishek1775@users.noreply.github.com>
2021-10-04 00:54:35 +0900

    #8.2 Conclusions

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,16 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 const API_KEY = "577dc4e69d991b157c442575f0584f9d";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          city.innerText = data.name;
+          weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
